3e3f1d311b2370087f9057ff4b4ba96a1fa8efd7
luoqp123456 <942882483@qq.com>
2020-11-18 15:24:09 +0800

    提交11-18

--- commen/readexcel.py
+++ commen/readexcel.py
@@ -1,3 +1,4 @@
+# -*- coding:utf8 -*-
 import xlrd
 from xlutils.copy import copy
 
@@ -16,19 +17,20 @@ def excel_to_list(xls_name, sheet_name):     #读取Excel表格sheet下的用例
 def get_test_data(data_list, casename):       #传递表格的全部数据，根据用例的名称获取数据
     for case_data in data_list:
         if casename == case_data['casename']:  # 如果字典数据中case_name与参数一致
-
             return case_data
             # 如果查询不到会返回None
 
 
 def write_excel(sheetindex, row, col, value):
     # excel_path = r"F:\ceshi.xls"  # 文件路径
-    excel_path = r"C:\Users\Administrator\PycharmProjects\pytest\data\ceshi.xls"
+    excel_path = r"C:\Users\Administrator\Desktop\添加学员.xlsx"
     # excel_path=unicode('D:\\测试.xls','utf-8')#识别中文路径
-    rbook = xlrd.open_workbook(excel_path, formatting_info=True)  # 打开文件
+    rbook = xlrd.open_workbook(excel_path)  # 打开文件
     wbook = copy(rbook)  # 复制文件并保留格式
     w_sheet = wbook.get_sheet(sheetindex)# 索引sheet表
     w_sheet.write(row, col, value)
     wbook.save(excel_path)  # 保存文件
 
 
+if __name__ == '__main__':
+    write_excel('Sheet1', 4, 0 ,'ceshi')
